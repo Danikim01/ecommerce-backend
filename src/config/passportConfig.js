3f11afed1ba70abd7c33d47162bea6df17fd6372
Danikim01 <dakim@fi.uba.ar>
2024-04-29 19:34:00 -0300

    client id y secret id agregados

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -8,8 +8,8 @@ import {createHash, isValidPassword} from '../utils/functionsUtil.js';
 const localStratergy = local.Strategy;
 const initializatePassport = () => {
 
-    const CLIENT_ID = "..."
-    const SECRET_ID = "..."
+    const CLIENT_ID = "Iv1.7db51b4a30ae7971"
+    const SECRET_ID = "a6d1dd0c9159d5dcff598ff5128ea2fe19629deb"
 
     passport.use(
         'github',
@@ -26,14 +26,13 @@ const initializatePassport = () => {
                 const newUser = {
                     first_name: profile.displayName.split(" ")[0] ?? profile._json.login,
                     last_name: profile.displayName.split(" ")[1] ?? profile.displayName.split(" ")[0],
-                    email: profile._json.email ?? "",
-                    age: 0,
-                    password: "",
+                    email: profile._json.email,
                 }
                 let result = await userModel.create(newUser);
                 done(null, result);
             } else {
                 console.log("El user ya existe!")
+                console.log("Email:",user.email)
                 done(null, user);
             }
         } catch(error) {
