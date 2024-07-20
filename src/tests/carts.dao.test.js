cbe33a62c106e67a442a72a6dc8a2096fbc8ab18
Danikim01 <dakim@fi.uba.ar>
2024-07-20 03:56:04 -0300

    get api/sessions/current test agregado y tests de cart fix

--- src/tests/carts.dao.test.js
+++ src/tests/carts.dao.test.js
@@ -1,9 +1,19 @@
 import { expect } from 'chai';
 import connection from '../utils/db.connection.js';
 
-import { cartsService } from '../repositories/index.js';
+import { cartsService,productsService } from '../repositories/index.js';
 
 const chai_expect = expect;
+const testProduct = {
+    title: "Test Product",
+    description: "Test Description",
+    code: "TEST123",
+    price: 100,
+    status: true,
+    stock: 10,
+    category: "Test",
+    owner: "user",
+}
 const testCart = {
     products: [],
     owner: "user",
@@ -13,12 +23,16 @@ connection();
 
 describe("Tests Cart DAO", () => {
      // Se ejecuta ANTES de comenzar el paquete de tests
-     before(function () {
+    before(async function () {
+        const result = await productsService.createProduct(testProduct);
+        testProduct._id = result._id;
     });
     // Se ejecuta ANTES de CADA test
     beforeEach(function () {});
     // Se ejecuta FINALIZADO el paquete de tests
-    after(function () {});
+    after(async function () {
+        const result = await productsService.deleteProduct(testProduct._id);
+    });
     // Se ejecuta FINALIZADO CADA text
     afterEach(function () {});
 
@@ -41,30 +55,30 @@ describe("Tests Cart DAO", () => {
     //test adding product to cart
     it("addProductToCart() debe retornar un objeto con el producto agregado al carrito", async () => {
         //warning, the added product should exist in the database otherwise the test will fail
-        const result = await cartsService.addProductToCart(testCart._id, "6685f73984896339899f8270");
+        const result = await cartsService.addProductToCart(testCart._id, testProduct._id);
         chai_expect(result).to.be.an("object");
         const addresult = await cartsService.getProductsFromCart(testCart._id);
         chai_expect(addresult).to.be.an("array");
-        chai_expect(addresult[0].product._id.toString()).to.be.equal("6685f73984896339899f8270");
+        chai_expect(addresult[0].product._id.toString()).to.be.equal(testProduct._id.toString());
         chai_expect(addresult[0].quantity).to.be.equal(1);
     });
 
 
     //test update cart with new products
     it("updateCart() debe retornar un objeto con el carrito actualizado", async () => {
-        await cartsService.updateCart(testCart._id, [{ product: "6685e82d540079dcada5809d", quantity: 2 }]);
+        await cartsService.updateCart(testCart._id, [{ product: testProduct._id, quantity: 2 }]);
         const updatedCart = await cartsService.getProductsFromCart(testCart._id);
         chai_expect(updatedCart).to.be.an("array");
-        chai_expect(updatedCart[0].product._id.toString()).to.be.equal("6685e82d540079dcada5809d");
+        chai_expect(updatedCart[0].product._id.toString()).to.be.equal(testProduct._id.toString());
         chai_expect(updatedCart[0].quantity).to.be.equal(2);
     });
 
     //test add product quantity
     it("updateProductQuantity() debe retornar un objeto con el producto actualizado", async () => {
-        await cartsService.updateProductQuantity(testCart._id, "6685e82d540079dcada5809d", 3);
+        await cartsService.updateProductQuantity(testCart._id, testProduct._id.toString(), 3);
         const updatedCart = await cartsService.getProductsFromCart(testCart._id);
         chai_expect(updatedCart).to.be.an("array");
-        chai_expect(updatedCart[0].product._id.toString()).to.be.equal("6685e82d540079dcada5809d");
+        chai_expect(updatedCart[0].product._id.toString()).to.be.equal(testProduct._id.toString());
         chai_expect(updatedCart[0].quantity).to.be.equal(3);
     });
 
--- src/tests/sessions.dao.test.js
+++ src/tests/sessions.dao.test.js
@@ -4,7 +4,7 @@ import { expect } from 'chai'
 import { usersService } from '../repositories/index.js';
 
 const testUser = { first_name: 'Juan', last_name: 'Perez', email: 'jperez@gmail.com', age: 18, password: 'abc445' };
-
+let cookie;
 describe("Tests Users", () => {
 
     after(async () => {
@@ -14,7 +14,7 @@ describe("Tests Users", () => {
     })
 
     it("POST /api/sessions/register deberia registrar un usuario",async () => {
-        await request(app).post("/api/sessions/register").send(testUser)
+        const res = await request(app).post("/api/sessions/register").send(testUser)
         const users = await usersService.getAllUsers();
         expect(users).to.be.an('array');
     })
@@ -22,9 +22,14 @@ describe("Tests Users", () => {
     it("POST /api/sessions/login deberia loguear un usuario",async () => {
         const result = await request(app).post("/api/sessions/login").send({email: testUser.email, password: testUser.password})
         const cookieData = result.headers['set-cookie'][0];
-        const cookie = { name: cookieData.split('=')[0], value: cookieData.split('=')[1] };
+        cookie = { name: cookieData.split('=')[0], value: cookieData.split('=')[1] };
         expect(cookieData).to.be.ok;
         expect(cookie.name).to.be.equals('auth');
         expect(cookie.value).to.be.ok;
+    })
+
+    it("GET /api/sessions/current deberia devolver el usuario logueado",async () => {
+        const res = await request(app).get('/api/sessions/current').set('Cookie', [`${cookie.name}=${cookie.value}`]);
+        expect(res).to.be.ok;
     })
 })
